230faffedf266c72ae91bd0e64c194ee1c0950e6
enaix <enaix@protonmail.com>
2023-09-03 15:23:49 +0300

    Add initial display support

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -1,17 +1,33 @@
 #include <Arduino.h>
+#include <U8g2lib.h>
 
 constexpr int BUTTON_PIN = 0;
 constexpr int ENCODER_PIN_A = 5;
 constexpr int ENCODER_PIN_B = 4;
 
+//#define DISPLAY_ENABLED
+constexpr int DISPLAY_CLK = 14;
+constexpr int DISPLAY_DATA = 13;
+constexpr int DISPLAY_CS = 15;
+constexpr int DISPLAY_RESET = 16;
+constexpr int DISPLAY_DC = 2;
+
 constexpr int BTN_DEBOUNCE_MS = 50;
 constexpr int BTN_CLICK_THRESH = 300;
 constexpr int BTN_DOUBLECLICK_THRESH = 150;
+
+#ifdef DISPLAY_ENABLED
+U8G2_SSD1306_128X64_NONAME_1_4W_HW_SPI u8g2(U8G2_R0, DISPLAY_CS, DISPLAY_DC, DISPLAY_RESET);
+#endif
 
 int enc_old_a;
 int btn_old;
 unsigned long btn_time;
 unsigned long btn_debounce;
+
+int display_millis = 0;
+int enc_pos = 0;
+bool btn_pressed = false;
 
 enum BTN_STATE
 {
@@ -42,6 +58,10 @@ void setup()
 	pinMode(ENCODER_PIN_B, INPUT_PULLUP);
 
 	Serial.begin(9600);
+	
+	#ifdef DISPLAY_ENABLED
+	u8g2.begin();
+	#endif
 
 	enc_old_a = digitalRead(ENCODER_PIN_A);
 	btn_old = digitalRead(BUTTON_PIN);
@@ -77,9 +97,11 @@ void printRotation(int btn_state=-1)
 	{
 		case ROTARY_STATE::RIGHT:
 			Serial.println(" up");
+			enc_pos++;
 			break;
 		case ROTARY_STATE::LEFT:
 			Serial.println(" down");
+			enc_pos--;
 			break;
 	}
 }
@@ -100,6 +122,29 @@ void printBtn()
 		case BTN_STATE::DOWN_DOUBLE:
 			Serial.println("sw_btn_doubleclick");
 	}
+}
+
+void draw_display()
+{
+	#ifdef DISPLAY_ENABLED
+
+	char buf[1];
+	u8g2.firstPage();
+	do
+	{
+		u8g2.setFont(u8g2_font_unifont_t_0_76);
+		u8g2.drawGlyph(0, 16, 9762);
+		u8g2.drawStr(18,16,"headless");
+		
+		u8g2.drawGlyph(0, 32, 9843 + enc_pos % 8);
+		if (btn_pressed)
+			u8g2.drawStr(16, 32, "*");
+
+		u8g2.setFont(u8g2_font_roentgen_nbp_tn);
+		u8g2.drawStr(0, 64, strcat(itoa(display_millis, buf, 10), "ns"));
+	} while (u8g2.nextPage());
+
+	#endif
 }
 
 void loop()
@@ -126,6 +171,11 @@ void loop()
 	enc_old_a = enc_a;
 
 	int btn_s = digitalRead(BUTTON_PIN);
+
+	if (btn_s == HIGH)
+		btn_pressed = true;
+	else
+		btn_pressed = false;
 
 	if (rotary != ROTARY_STATE::IDLE)
 	{
@@ -208,6 +258,10 @@ void loop()
 			}
 		}
 	}
+
+	size_t draw_time = millis();
+	draw_display(); // todo subtract from millis
+	display_millis = millis() - draw_time;
 
 	delay(1);
 }
